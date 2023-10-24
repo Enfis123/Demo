0f47b831ba9e1d99c78d4ae4746efbb046f71398
Victor <victor.moreno01@epn.edu.ec>
2023-10-23 19:11:54 -0500

    Datos2

--- client/public/scripts/menus.js
+++ client/public/scripts/menus.js
@@ -1,6 +1,6 @@
 // Obtenemos todos los elementos con la clase "menu-item"
 const menuItems = document.querySelectorAll('.menu-item');
-const contentContainer = document.querySelector('.content-container'); // Elemento de contenedor
+const mainContent = document.querySelector('.main-content'); // Elemento de contenido principal
 
 // Asignamos un evento 'click' a cada elemento del menú
 menuItems.forEach(item => {
@@ -8,22 +8,19 @@ menuItems.forEach(item => {
         // Obtenemos el texto del elemento del menú seleccionado
         const selectedMenuItem = item.textContent;
 
-        // Borramos el contenido existente en el contenedor
-        contentContainer.innerHTML = '';
-
-        // Cargamos el contenido correspondiente en el contenedor según la selección
+        // Cargamos el contenido correspondiente en el contenido principal según la selección
         if (selectedMenuItem.includes("Crear Usuario")) {
             // Cargar el contenido para "Crear Usuario"
-            contentContainer.innerHTML = '<h3>Contenido de Crear Usuario</h3>';
+            mainContent.innerHTML = '<h3>Contenido de Crear Usuario</h3>';
         } else if (selectedMenuItem.includes("Visualizar Usuarios")) {
             // Cargar el contenido para "Visualizar Usuarios"
-            contentContainer.innerHTML = '<h3>Contenido de Visualizar Usuarios</h3>';
+            mainContent.innerHTML = '<h3>Contenido de Visualizar Usuarios</h3>';
         } else if (selectedMenuItem.includes("Crear Variable")) {
             // Cargar el contenido para "Crear Variable"
-            contentContainer.innerHTML = '<h3>Contenido de Crear Variable</h3>';
+            mainContent.innerHTML = '<h3>Contenido de Crear Variable</h3>';
         } else if (selectedMenuItem.includes("Visualizar Variable")) {
             // Cargar el contenido para "Visualizar Variable"
-            contentContainer.innerHTML = '<h3>Contenido de Visualizar Variable</h3>';
+            mainContent.innerHTML = '<h3>Contenido de Visualizar Variable</h3>';
         }
     });
 });
